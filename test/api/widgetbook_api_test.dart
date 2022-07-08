49031703abb5fbb9e1458be1144c39d0c0d68fff
Therenca Mureithi <therencamureithi@gmail.com>
2022-07-08 14:23:54 +0300

    populate

--- test/api/widgetbook_api_test.dart
+++ test/api/widgetbook_api_test.dart
@@ -3,28 +3,27 @@ import 'package:mocktail/mocktail.dart';
 import 'package:widgetbook_challenge/api/widgetbook_api.dart';
 
 class MockWidgetbookApiSuccess extends Mock implements WidgetbookApi {
-	@override
-	Future<String> welcomeToWidgetbook({
+  @override
+  Future<String> welcomeToWidgetbook({
     required String message,
   }) async {
-			return 'Hello $message';
+    return 'Hello $message';
   }
 }
 
 class MockWidgetbookApiFailure extends Mock implements WidgetbookApi {
-
-	@override
-	Future<String> welcomeToWidgetbook({
+  @override
+  Future<String> welcomeToWidgetbook({
     required String message,
   }) async {
-			return throw UnexpectedException();
+    return throw UnexpectedException();
   }
 }
 
-void main(){
-	test('welcome to widgetbook when called and is successful', () async {
-		final mock = MockWidgetbookApiSuccess();
-		final result = await mock.welcomeToWidgetbook(message: 'john');
-		expect(result, 'Hello john');
-	});
+void main() {
+  test('welcome to widgetbook when called and is successful', () async {
+    final mock = MockWidgetbookApiSuccess();
+    final result = await mock.welcomeToWidgetbook(message: 'john');
+    expect(result, 'Hello john');
+  });
 }
